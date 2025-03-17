5bb357f76f8159d1c8ee61c7b2a1ba2603da4972
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2025-03-17 02:27:25 +0000

    updates to fix heroku

--- setup.py
+++ setup.py
@@ -3,9 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="mini-cms",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
         "requests",
+        "fastapi",
+        "uvicorn",
+        "python-dotenv",
+        "fastapi-sso",
+        "jinja2",
+        "python-multipart",
     ],
+    python_requires=">=3.8",
 )
